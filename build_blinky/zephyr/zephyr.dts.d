31b5d24e80f509ca2e7738e5c35a0e26e6d7217f
upgautamvt <upgautam@vt.edu>
2025-07-29 12:04:07 -0400

    blinky app testing worked

---
+++ build_blinky/zephyr/zephyr.dts.d
@@ -0,0 +1,33 @@
+empty_file.o: \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/misc/empty_file.c \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/boards/nordic/nrf52840dk/nrf52840dk_nrf52840.dts \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/common/mem.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/vendor/nordic/nrf_common.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_macro.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_internal.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_loops.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_listify.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/sensor/qdec_nrf.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/common/freq.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/arm/nordic/override.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/nrf-saadc-v3.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/nrf-saadc-v2.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/nrf-saadc.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/include/zephyr/dt-bindings/regulator/nrf5x.h \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/dts/vendor/nordic/nrf52840_partition.dtsi \
+ /home/upgautam/CLionProjects/BlueFortACM2025/zephyrproject/zephyr/boards/nordic/nrf52840dk/nrf52840dk_nrf52840-pinctrl.dtsi
